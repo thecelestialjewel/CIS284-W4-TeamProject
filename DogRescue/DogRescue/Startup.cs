27b39461d36d146e6a345e6dd14d7e42ce4a8c2e
thecelestialjewel <christinemjordan6@gmail.com>
2020-10-12 14:49:45 -0700

    initial check in

---
+++ DogRescue/DogRescue/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DogRescue.Startup))]
+namespace DogRescue
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
