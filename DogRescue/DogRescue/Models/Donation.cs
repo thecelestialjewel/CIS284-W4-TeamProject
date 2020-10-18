4b56499e875aeb78c1c9989eb92930a10a447777
thecelestialjewel <christinemjordan6@gmail.com>
2020-10-18 10:09:03 -0700

    Still working on donations page and database connection.

---
+++ DogRescue/DogRescue/Models/Donation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DogRescue.Models
+{
+    public class Donation //model that holds donation information
+    {
+        public int Id { get; set; }//autoproperty
+        public string Donator { get; set; } //autoproperty for person who donates
+        public decimal Amount { get; set; } //autoproperty for donaton amount
+    }
+
+    public class DonationsDbContext : DbContext //how donations communicates with database
+    {
+        public DbSet<Donation> Donations { get; set; }//C# representation of donations database
+    }
+}
